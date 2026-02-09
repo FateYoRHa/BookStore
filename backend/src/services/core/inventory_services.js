45cb46263e9e381ffb98750ddec6d973df44b535
FateYoRHa <fagyan.att@gmail.com>
2026-02-09 16:01:56 +0800

    feat(inventory): add inventory init to service

---
+++ backend/src/services/core/inventory_services.js
@@ -0,0 +1,8 @@
+import {Inventory} from "../../model/index.js";
+
+export async function initializeInventory(bookId, quantity) {
+  return Inventory.create({
+    book: bookId,
+    quantity,
+  });
+}
