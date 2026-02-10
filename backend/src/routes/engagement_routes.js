73644c3735c5549c1af344df9e795372f03329fa
FateYoRHa <fagyan.att@gmail.com>
2026-02-10 10:12:05 +0800

    feat(review): connect controller to route

--- backend/src/routes/engagement_routes.js
+++ backend/src/routes/engagement_routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import * as wishlist from "../controllers/engagement/wishlists_controller.js"
+import * as review from "../controllers/engagement/reviews_controller.js";
 
 const router = express.Router()
 
@@ -8,6 +9,8 @@ const router = express.Router()
 router.put("/wishlist/add/", wishlist.addToWishlist);
 router.put("/wishlist/remove/", wishlist.removeFromWishlist);
 
-
+// REVIEW
+router.post("/review/create/", review.addReview)
+router.put("/review/update/:id", review.updateReview)
 
 export default router
