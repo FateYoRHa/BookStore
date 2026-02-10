9f09c5170dd4bf51ca66cd42fdee2e86e5592907
FateYoRHa <fagyan.att@gmail.com>
2026-02-11 07:22:06 +0800

    refactor(server): add newsletter route

--- backend/src/routes/engagement_routes.js
+++ backend/src/routes/engagement_routes.js
@@ -1,16 +1,20 @@
 import express from "express";
 
-import * as wishlist from "../controllers/engagement/wishlists_controller.js"
+import * as wishlist from "../controllers/engagement/wishlists_controller.js";
 import * as review from "../controllers/engagement/reviews_controller.js";
+import * as newsletter from "../controllers/engagement/newsletters_controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
 // WISHLIST
 router.put("/wishlist/add/", wishlist.addToWishlist);
 router.put("/wishlist/remove/", wishlist.removeFromWishlist);
 
 // REVIEW
-router.post("/review/create/", review.addReview)
-router.put("/review/update/:id", review.updateReview)
+router.post("/review/create/", review.addReview);
+router.put("/review/update/:id", review.updateReview);
 
-export default router
+// NEWSLETTER
+router.post("/newsletter/create/", newsletter.createNewsLetter);
+
+export default router;
