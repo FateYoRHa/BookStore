0a1106829d355c3702c3cde4808b40747a96618d
FateYoRHa <fagyan.att@gmail.com>
2026-02-11 11:35:50 +0800

    refactor(cart): connect cart to route

--- backend/src/routes/commerce_routes.js
+++ backend/src/routes/commerce_routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 
 import * as orders from "../controllers/commerce/orders_controller.js";
+import * as cart from "../controllers/commerce/carts_controller.js";
 
 const router = express.Router();
 
+// CARTS
+router.put("/cart/add", cart.addToCart)
+router.put("/cart/remove", cart.removeFromCart);
 // ORDERS
 router.post("/orders/add", orders.addOrder);
 router.put("/orders/update/:id", orders.updateOrder);
