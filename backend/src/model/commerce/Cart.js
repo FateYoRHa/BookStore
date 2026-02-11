b58bfc29d1bff2635ddc91fe3389f9901b8e599e
FateYoRHa <fagyan.att@gmail.com>
2026-02-11 11:34:40 +0800

    refactor(model): update cart model to mirror cart controller + service

--- backend/src/model/commerce/Cart.js
+++ backend/src/model/commerce/Cart.js
@@ -1,16 +1,22 @@
 import { mongoose, Schema } from "mongoose";
-const cartItemSchema = new Schema({
-  book: { type: mongoose.Schema.Types.ObjectId, ref: "Book", required: true },
-  quantity: { type: Number, min: 1, required: true },
-  priceSnapshot: { type: Number, required: true },
-});
 
-const cartSchema = new mongoose.Schema(
+const cartSchema = new Schema(
   {
     customer: { type: mongoose.Schema.Types.ObjectId, ref: "Customer" }, // nullable for guest
-    items: [cartItemSchema],
+    items: [
+      {
+        book: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Book",
+          required: true,
+        },
+        quantity: { type: Number, min: 1, required: true },
+        priceSnapshot: { type: Number, required: true },
+      },
+    ],
   },
   { timestamps: true },
 );
+cartSchema.index({ customer: 1 }, { unique: true });
 
 export default mongoose.model("Cart", cartSchema);
