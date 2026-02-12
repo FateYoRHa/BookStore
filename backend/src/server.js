830a720a1e66cd00add2a4467c9c3f25f70a6349
FateYoRHa <fagyan.att@gmail.com>
2026-02-12 21:19:55 +0800

    feat(server): import cookie-parser

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 
 import { connectDB } from "./config/db.js";
 import rateLimit from "./middleware/rateLimiterer.js";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./auth/auth_routes.js";
 import commerceRoutes from "./routes/commerce_routes.js";
@@ -20,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(rateLimit);
-
+app.use(cookieParser());
 // AUTH
 app.use("/auth/", authRoutes);
 //routes
