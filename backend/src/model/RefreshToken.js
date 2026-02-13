86ef6497bd9b4c4a2f4ab43cad38d59746d57e15
FateYoRHa <fagyan.att@gmail.com>
2026-02-13 12:12:38 +0800

    feat(auth): add model for storing refresh tokens

---
+++ backend/src/model/RefreshToken.js
@@ -0,0 +1,31 @@
+import { Schema, mongoose } from "mongoose";
+import bcrypt from "bcrypt";
+const refreshTokenSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    token: {
+      type: String,
+      required: true,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+refreshTokenSchema.pre("save", async function () {
+  if (!this.isModified("token")) return;
+  // Only hash if the password has been modified
+  this.token = await bcrypt.hash(this.token, 12); // 12 is the salt rounds
+});
+// Method to compare the refresh tokens with the stored hash
+refreshTokenSchema.methods.compareRefreshToken = async function (token) {
+  return await bcrypt.compare(token, this.token);
+};
+export const RefreshToken = mongoose.model("RefreshToken", refreshTokenSchema);
