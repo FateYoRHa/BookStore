5b9daeef01c3601f9721e73ba12829139d8736a1
FateYoRHa <fagyan.att@gmail.com>
2026-02-13 10:09:50 +0800

    refactor(auth): add login and its validataor to route

--- backend/src/auth/auth_routes.js
+++ backend/src/auth/auth_routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import * as auth from "./auth_controller.js";
 
-import userValidation from "./auth_validation.js";
+import { validateUser, validateLogin } from "./auth_validation.js";
 import { validateRequest } from "../middleware/validateRequest.js";
 
 const router = express.Router();
 
-router.post("/register", userValidation, validateRequest, auth.register);
+router.post("/register", validateUser, validateRequest, auth.register);
+router.post("/login", validateLogin, validateRequest, auth.login);
 
 export default router;
