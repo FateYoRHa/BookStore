56163d5847cdd581ed3701a0622b24a42b849454
FateYoRHa <fagyan.att@gmail.com>
2026-02-13 12:12:04 +0800

    feat(auth): add controller and service for logout/refresh

--- backend/src/auth/auth_controller.js
+++ backend/src/auth/auth_controller.js
@@ -7,15 +7,17 @@ export async function register(req, res) {
     const { user, accessToken, refreshToken } =
       await authService.registerService({ email, password });
 
-    res
-      .status(201)
-      .json({ access_token: accessToken, refresh_token: refreshToken });
+    res.status(201).json({
+      user: user,
+      access_token: accessToken,
+      refresh_token: refreshToken,
+    });
   } catch (error) {
-      if (error.code === 11000) {
-        return res.status(400).json({
-          message: "Duplicate value detected.",
-        });
-      }
+    if (error.code === 11000) {
+      return res.status(400).json({
+        message: "Duplicate value detected.",
+      });
+    }
 
     console.log("Error register", error);
     res
@@ -49,10 +51,41 @@ export async function login(req, res) {
   }
 }
 
+export async function refresh(req, res) {
+  try {
+    // get refresh token from cookie and send to refresh service
+    const {accessToken, refreshToken} = await authService.refreshService(
+      req.cookies.refreshToken,
+    );
+    // set new cookie
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+    res.status(200).json({ access_token: accessToken });
+  } catch (error) {
+    console.log("Error refresh", error.message || error);
+    res
+      .status(error.status || 500)
+      .json({ message: error.message || "Internal Server Error" });
+  }
+}
+
 export async function logout(req, res) {
   try {
+    await authService.logoutService(req.cookies.refreshToken);
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+    });
+    return res.status(200).json({
+      message: "Logged out successfully",
+    });
   } catch (error) {
-    console.log("Error register", error);
-    res.status(500).json({ message: "Internal Server Error" });
+    console.log("Error logout", error);
+    res.status(500).json({ message: "Logout Failed" });
   }
 }
--- backend/src/auth/auth_services.js
+++ backend/src/auth/auth_services.js
@@ -1,5 +1,10 @@
 import { User } from "../model/index.js";
-import { signAccessToken, signRefreshToken } from "../utis/token.js";
+import { RefreshToken } from "../model/RefreshToken.js";
+import {
+  signAccessToken,
+  signRefreshToken,
+  verifyRefreshToken,
+} from "../utis/token.js";
 
 import { createCustomerService } from "../services/core/customer_services.js";
 
@@ -44,6 +49,89 @@ export async function loginService(login) {
   // ISSUE TOKEN
   const accessToken = signAccessToken(user);
   const refreshToken = signRefreshToken(user);
+  // Store refresh token connected to user id
+  await RefreshToken.create({
+    user: user._id,
+    token: refreshToken,
+    expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+  });
 
   return { accessToken, refreshToken };
+}
+
+export async function refreshService(refreshToken) {
+  try {
+    if (!refreshToken) {
+      const error = new Error("Token not recognized");
+      error.status = 401;
+      throw error;
+    }
+    // verify refresh token
+    const payload = verifyRefreshToken(refreshToken);
+    // find token using user id
+    const token = await RefreshToken.findOne({ user: payload.sub });
+    if (!token) {
+      const error = new Error("Token not recognized");
+      error.status = 403;
+      throw error;
+    }
+    if (token.expiresAt < new Date()) {
+      const error = new Error("Token expired");
+      error.status = 401;
+      throw error;
+    }
+    // compare token with hashed token
+    const storedToken = await token.compareRefreshToken(refreshToken);
+    if (!storedToken) {
+      const error = new Error("Token not recognized");
+      error.status = 403;
+      throw error;
+    }
+    // REFRESH ROTATION
+    await RefreshToken.deleteOne({ token: storedToken });
+
+    // check db for user
+    const user = await User.findById(payload.sub);
+    // ISSUE TOKENs
+    const accessToken = signAccessToken(user);
+    const refreshToken = signRefreshToken(user);
+    await RefreshToken.create({
+      user: user._id,
+      token: refreshToken,
+      expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    console.log("refresh service error", error.message || error);
+    throw error;
+  }
+}
+
+export async function logoutService(refreshToken) {
+  try {
+    if (!refreshToken) return;
+    // verify refresh token
+    const payload = verifyRefreshToken(refreshToken);
+    // find all tokens that belong to user
+    const tokens = await RefreshToken.find({ user: payload.sub });
+    if (!tokens) return;
+    let matchedToken = null;
+    // loop through to find correct token
+    for (const t of tokens) {
+      // compare each hash
+      const isMatch = await t.compareRefreshToken(refreshToken);
+      if (isMatch) {
+        // match correct one
+        matchedToken = t;
+        break;
+      }
+    }
+
+    if (!matchedToken) throw error;
+    return await RefreshToken.deleteOne({ _id: matchedToken._id });
+  } catch (error) {
+    console.log("Error logout service", error.message || error);
+    throw error;
+  }
 }
