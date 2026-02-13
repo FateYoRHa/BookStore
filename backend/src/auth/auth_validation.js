27efb94cd2450010280c89a83500c750e9bb4420
FateYoRHa <fagyan.att@gmail.com>
2026-02-13 10:09:09 +0800

    refactor(auth): add login validation

--- backend/src/auth/auth_validation.js
+++ backend/src/auth/auth_validation.js
@@ -1,11 +1,12 @@
 import { body } from "express-validator";
 
-const validateUser = [
+export const validateUser = [
   body("email")
     .trim()
     .normalizeEmail()
-    .isEmail().withMessage("Please enter a valid email address."),
-    
+    .isEmail()
+    .withMessage("Please enter a valid email address."),
+
   body("password")
     .isLength({ min: 8 })
     .withMessage("Password must have at least 8 characters long")
@@ -18,5 +19,15 @@ const validateUser = [
     .matches(/[!@#$%^&*_\-]/)
     .withMessage("Password must contain at least one special character"),
 ];
+export const validateLogin = [
+  body("email")
+    .notEmpty()
+    .withMessage("Email cannot be empty.")
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage("Please enter a valid email address."),
+  body("password").notEmpty().withMessage("Password cannot be empty."),
+];
 
-export default validateUser
+// export default { validateUser, validateLogin };
