b1ecc90260f7bf1c1632377e876553a5a6182407
FateYoRHa <fagyan.att@gmail.com>
2026-02-14 10:00:30 +0800

    refactor(customer): customer now use req.user.id   - controller use id from authenticate   - get customer order moved to service

--- backend/src/controllers/core/customers_controller.js
+++ backend/src/controllers/core/customers_controller.js
@@ -1,15 +1,14 @@
-import * as customerService from "../../services/core/customer_services.js"
-import * as orderService from "../../services/commerce/order_services.js"
+import * as customerService from "../../services/core/customer_services.js";
 
 export async function getCustomer(req, res) {
   try {
-    const customer = await customerService.getCustomerService(req.params.id)
-
-    const orders = await orderService.getOrderService(req.params.id);
+    const { customer, orders } = await customerService.getCustomerService(
+      req.user.id,
+    );
 
     res.status(200).json({ customer, orders });
   } catch (error) {
-    console.log("Error retrieving customer", error)
+    console.log("Error retrieving customer", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
--- backend/src/services/core/customer_services.js
+++ backend/src/services/core/customer_services.js
@@ -1,16 +1,20 @@
 import { Customer, Order } from "../../model/index.js";
-
+import * as orderService from "../../services/commerce/order_services.js";
 export async function getCustomerService(id) {
   try {
-    const customer = await Customer.findById(id).populate("user", "email role");
+    const customer = await Customer.findOne({ user: id }).populate(
+      "user",
+      "email role",
+    );
+    const orders = await orderService.getOrderService(customer.id);
     if (!customer) {
       const error = new Error("Customer not found.");
       error.status = 404;
       throw error;
     }
-    return customer;
+    return {customer, orders};
   } catch (error) {
-    
+    throw error
   }
 }
 
