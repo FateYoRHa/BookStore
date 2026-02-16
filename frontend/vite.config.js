5abdaa701e4133f530427485b35f9a71d6ee7ec0
FateYoRHa <fagyan.att@gmail.com>
2026-02-16 12:02:51 +0800

    refactor: fix "localhost did not send data"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,14 +10,14 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
   server: {
     host: "0.0.0.0",
     port: 5173,
     strictPort: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
