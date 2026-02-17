0d38dc5b8260426f353d688507f83a2d8845cbe8
FateYoRHa <fagyan.att@gmail.com>
2026-02-17 11:11:53 +0800

    feat: add navbar, wrap style for all pages

--- frontend/src/app/Providers.jsx
+++ frontend/src/app/Providers.jsx
@@ -3,6 +3,8 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { queryClient } from "../services/queryClient.js";
 
+import NavBar from "@/components/NavBar.jsx";
+
 // This file wraps GLOBAL providers.
 // Anything that should exist across the whole app goes here.
 // if you add new imports, package etc. add them here
@@ -11,16 +13,16 @@ export default function Providers({ children }) {
   return (
     // Enables React Query across the entire app
     <QueryClientProvider client={queryClient}>
-      
       {/* Enables routing system */}
       <BrowserRouter>
-        
-        {/* Renders whatever component is passed (AppRouter) */}
-        {children}
+        <NavBar />
+        <div className="min-h-screen bg-muted">
+          {/* Renders whatever component is passed (AppRouter) */}
+          {children}
+        </div>
 
         {/* Toast notification system */}
         <Toaster />
-
       </BrowserRouter>
     </QueryClientProvider>
   );
