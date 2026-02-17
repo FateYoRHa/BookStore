21b9ef58c9ed3761929e6a5d22b68f7baf854ec3
FateYoRHa <fagyan.att@gmail.com>
2026-02-17 11:12:51 +0800

    feat(routes): add home, authors

--- frontend/src/app/Router.jsx
+++ frontend/src/app/Router.jsx
@@ -1,12 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "../features/auth/pages/LoginPage";
+
+import HomePage from "@/features/HomePage";
+
 import Books from "@/features/books/pages/Books";
+
+import Author from "@/features/authors/pages/Author";
 
 export default function AppRouter() {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />} />
+
       <Route path="/login" element={<LoginPage />} />
+
       <Route path="/books" element={<Books />} />
+
+      <Route path="/authors" element={<Author />} />
     </Routes>
   );
 }
