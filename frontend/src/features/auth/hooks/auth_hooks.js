0be8e36c658cbd8b050dce912c65ed5f8e50adf8
FateYoRHa <fagyan.att@gmail.com>
2026-02-19 12:11:35 +0800

    feat: add signup, logout hooks

---
+++ frontend/src/features/auth/hooks/auth_hooks.js
@@ -0,0 +1,72 @@
+import { useMutation } from "@tanstack/react-query";
+import { loginRequest, signupRequest, logoutRequest } from "../api/auth.js";
+import { useAuthStore } from "../store/authStore.js";
+import toast from "react-hot-toast";
+
+export const useLogin = () => {
+  // const setUser = useAuthStore((state) => state.setUser);
+  const login = useAuthStore((state) => state.login);
+  return useMutation({
+    // This is the function that performs the request
+    mutationFn: loginRequest,
+
+    // Runs when request succeeds
+    onSuccess: (data) => {
+      // Save user globally
+      login(data);
+      // login(data.user, data.token);
+      toast.success("Login successful");
+    },
+
+    // Runs when request fails
+    onError: (error) => {
+      if (error.response) {
+        // Server responded with error
+        toast.error(error.response.data?.message || "Request failed");
+      } else if (error.request) {
+        // Request made but no response
+        toast.error("Server not responding");
+      } else {
+        // Something else
+        toast.error("Unexpected error occurred");
+      }
+    },
+  });
+};
+export const useSignup = () => {
+  const login = useAuthStore((state) => state.login);
+
+  return useMutation({
+    mutationFn: signupRequest,
+
+    onSuccess: (data) => {
+      login(data);
+      toast.success("Account created successfully");
+    },
+
+
+  });
+};
+export const useLogout = () => {
+  const logout = useAuthStore((state) => state.logout);
+  return useMutation({
+    mutationFn: logoutRequest,
+
+    onSuccess: () => {
+      logout();
+      toast.success("Logged out");
+    },
+    onError: (error) => {
+      if (error.response) {
+        // Server responded with error
+        toast.error(error.response.data?.message || "Logout failed");
+      } else if (error.request) {
+        // Request made but no response
+        toast.error("Server not responding");
+      } else {
+        // Something else
+        toast.error("Unexpected error occurred");
+      }
+    },
+  });
+};
