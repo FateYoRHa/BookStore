a9d68ff316397d8338b49850ff8dc91ceaf5c442
FateYoRHa <fagyan.att@gmail.com>
2026-02-19 12:08:13 +0800

    feat: add signup validaiton schema

--- frontend/src/features/auth/schema.js
+++ frontend/src/features/auth/schema.js
@@ -6,5 +6,28 @@ import { z } from "zod";
 export const loginSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(8, "Password must be at least 8 characters"),
-
 });
+
+export const signupSchema = z
+  .object({
+    email: z.string().email("Invalid email address"),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .regex(/[a-z]/, { message: "Must contain at least one lowercase letter" })
+      .regex(/[A-Z]/, { message: "Must contain at least one uppercase letter" })
+      // Matches at least one special character (\W matches non-word, \W_ includes underscore)
+      .regex(/[\W_]/, {
+        message: "Must contain at least one special character",
+      }),
+    confirmPassword: z.string(),
+  })
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "The passwords do not match",
+        path: ["confirmPassword"], // Associates the error with the confirmPassword field
+      });
+    }
+  });
