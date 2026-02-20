c1b3c86c3586b848d0d2a294d7d3d334cc1468bd
FateYoRHa <fagyan.att@gmail.com>
2026-02-20 19:08:49 +0800

    refactor: change getBook api to properly get book

--- frontend/src/features/books/api/books.js
+++ frontend/src/features/books/api/books.js
@@ -1,14 +1,13 @@
 import api from "../../../services/axios.js";
 
 export const getBooksRequest = async (filters) => {
-
   const response = await api.get("/core/books", {
     params: filters, // ✅ THIS IS REQUIRED
   });
   return response.data;
 };
 
-export const getBookRequest = async (data) => {
-  const book = await api.get("/core/books",data);
-  return book;
+export const getBookRequest = async (id) => {
+  const book = await api.get(`/core/books/${id}`);
+  return book.data;
 };
