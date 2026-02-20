4f85d5c5cc04f19cbda24fbbd2a8657f26f6d1b0
FateYoRHa <fagyan.att@gmail.com>
2026-02-20 12:21:31 +0800

    feat: add filter and send to api

--- frontend/src/features/books/hooks/book_hooks.js
+++ frontend/src/features/books/hooks/book_hooks.js
@@ -1,6 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBooksRequest } from "../api/books.js";
 
-export const useBooks = () => {
-  return useQuery({ queryKey: ["books"], queryFn: getBooksRequest });
+export const useBooks = (filters) => {
+  return useQuery({
+    queryKey: ["books", filters],
+    queryFn: () => getBooksRequest(filters),
+  });
 };
