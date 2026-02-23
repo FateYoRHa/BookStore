6855fcb3e7ec3490e47059a7c409466167e68810
FateYoRHa <fagyan.att@gmail.com>
2026-02-23 13:39:20 +0800

    feat: add cart route

--- frontend/src/app/Router.jsx
+++ frontend/src/app/Router.jsx
@@ -6,6 +6,8 @@ import HomePage from "@/features/homepage/pages/HomePage";
 import Books from "@/features/books/pages/Books";
 import BookDetails from "@/features/books/pages/BookDetails";
 
+import Cart from "@/features/cart/pages/Cart";
+
 import Author from "@/features/authors/pages/Author";
 
 export default function AppRouter() {
@@ -14,6 +16,8 @@ export default function AppRouter() {
       <Route path="/" element={<HomePage />} />
 
       <Route path="/login" element={<LoginPage />} />
+
+      <Route path="/cart" element={<Cart />} />
 
       <Route path="/books" element={<Books />} />
       <Route path="/books/:id" element={<BookDetails />} />
