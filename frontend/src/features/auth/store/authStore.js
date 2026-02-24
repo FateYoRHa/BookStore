c8173fd813afec417c76a6914e5aae6d2b6920c8
FateYoRHa <fagyan.att@gmail.com>
2026-02-24 11:54:29 +0800

    refactor: separate accessToken and user, use jwt-decoder to decode user

--- frontend/src/features/auth/store/authStore.js
+++ frontend/src/features/auth/store/authStore.js
@@ -1,16 +1,33 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import { jwtDecode } from "jwt-decode";
 
 // Global state container for authentication.
 
 export const useAuthStore = create(
   persist(
     (set) => ({
+      accessToken: null,
       user: null, // Stores logged in user
 
-      login: (user) => set({ user }), // Updates user
+      login: (data) => {
+        // accessToken contains user data
+        const decoded = jwtDecode(data.accessToken); // decode accesstoken
+        set({
+          accessToken: data.accessToken,
+          user: decoded, // set decoded accessToken as user
+        });
+      },
+      logout: () =>
+        set({
+          accessToken: null, // Clears accessToken
+          user: null, // Clears user
+        }),
 
-      logout: () => set({ user: null }), // Clears user
+      setAccessToken: (token) => {
+        const decoded = jwtDecode(token); // decode accesstoken
+        set({ accessToken: token, user: decoded });
+      },
     }),
     {
       name: "auth-storage",
