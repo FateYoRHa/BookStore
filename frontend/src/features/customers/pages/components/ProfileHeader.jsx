6ed15cb431ef5f9f52179211f50181c503b01c30
FateYoRHa <fagyan.att@gmail.com>
2026-02-26 14:17:41 +0800

    feat: profile header initial commit, displays customer

---
+++ frontend/src/features/customers/pages/components/ProfileHeader.jsx
@@ -0,0 +1,67 @@
+import { useContext } from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Camera, Calendar, Mail, MapPin } from "lucide-react";
+import { EditingContext } from "../../context/customer_context";
+import { useAuthStore } from "@/features/auth/store/authStore";
+const ProfileHeader = () => {
+  const { setEditing } = useContext(EditingContext);
+  const customerData = useAuthStore((state) => state.customer);
+  const customer = customerData?.customer
+  const dateObj = new Date(customer?.createdAt);
+  const date = dateObj.toLocaleDateString(undefined, {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const address = customer?.address[0];
+  return (
+    <Card>
+      <CardContent>
+        <div className="flex flex-col items-start gap-6 md:flex-row md:items-center">
+          <div className="relative">
+            <Avatar className="h-24 w-24">
+              <AvatarImage src={customer?.image} alt={customer?.name} />
+              <AvatarFallback className="text-2xl">
+                {customer?.name}
+              </AvatarFallback>
+            </Avatar>
+            <Button
+              size="icon"
+              variant="outline"
+              className="absolute -right-2 -bottom-2 h-8 w-8 rounded-full">
+              <Camera />
+            </Button>
+          </div>
+          <div className="flex-1 space-y-2">
+            <div className="flex flex-col gap-2 md:flex-row md:items-center">
+              <h1 className="text-2xl font-bold">{customer?.name}</h1>
+              <Badge variant="secondary">{customer?.customerCode}</Badge>
+            </div>
+            <div className="text-muted-foreground flex flex-wrap gap-4 text-sm">
+              <div className="flex items-center gap-1">
+                <Mail className="size-4" />
+                {customer?.user.email}
+              </div>
+              <div className="flex items-center gap-1">
+                <MapPin className="size-4" />
+                {address?.country}, {address?.city}
+              </div>
+              <div className="flex items-center gap-1">
+                <Calendar className="size-4" />
+                Joined {date}
+              </div>
+            </div>
+          </div>
+          <Button variant="default" onClick={() => setEditing(true)}>
+            Edit Profile
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ProfileHeader;
