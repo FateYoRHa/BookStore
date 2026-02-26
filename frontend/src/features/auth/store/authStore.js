8ed586282e32e287d3edce880f7f4ef4224bdab8
FateYoRHa <fagyan.att@gmail.com>
2026-02-26 14:16:07 +0800

    refactor: login now stores customer globally and logout clears all

--- frontend/src/features/auth/store/authStore.js
+++ frontend/src/features/auth/store/authStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import api from "@/services/axios";
 import { jwtDecode } from "jwt-decode";
 
 // Global state container for authentication.
@@ -9,19 +10,26 @@ export const useAuthStore = create(
     (set) => ({
       accessToken: null,
       user: null, // Stores logged in user
+      customer: null, // Stores customer
 
-      login: (data) => {
+      login: async (data) => {
         // accessToken contains user data
         const decoded = jwtDecode(data.accessToken); // decode accesstoken
         set({
           accessToken: data.accessToken,
           user: decoded, // set decoded accessToken as user
         });
+        
+        const res = await api.get("/core/customers/me");
+        set({
+          customer: res.data,
+        });
       },
       logout: () =>
         set({
           accessToken: null, // Clears accessToken
           user: null, // Clears user
+          customer: null, // clear customer
         }),
 
       setAccessToken: (token) => {
