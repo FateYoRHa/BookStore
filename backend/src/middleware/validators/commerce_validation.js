034d35fba9abafdf43d679519376bff5fa669f4c
FateYoRHa <fagyan.att@gmail.com>
2026-02-27 11:53:20 +0800

    refactor: add cart book limit message

--- backend/src/middleware/validators/commerce_validation.js
+++ backend/src/middleware/validators/commerce_validation.js
@@ -2,7 +2,7 @@ import { body } from "express-validator";
 
 export const addToCartValidator = [
   body("bookId").isMongoId(),
-  body("quantity").isInt({ min: 1, max: 10 }),
+  body("quantity").isInt({ min: 1, max: 10 }).withMessage("Up to 10 books are allowed."),
 ];
 
 export const paymenMethod = [
