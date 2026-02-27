f3af28a2fcb1f8f43bbedee50ef2be83b256612f
FateYoRHa <fagyan.att@gmail.com>
2026-02-27 10:28:32 +0800

    refactor: add update form submit

--- frontend/src/features/customers/pages/components/PersonalInfoTab.jsx
+++ frontend/src/features/customers/pages/components/PersonalInfoTab.jsx
@@ -1,4 +1,7 @@
 import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { cn } from "@/lib/utils";
 import {
   Card,
   CardHeader,
@@ -6,20 +9,58 @@ import {
   CardDescription,
   CardTitle,
 } from "@/components/ui/card";
+import FormFieldError from "@/components/forms/FormFieldError";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import toast from "react-hot-toast";
 
 import { EditingContext } from "../../context/customer_context";
 import { useAuthStore } from "@/features/auth/store/authStore";
+import { updateCustomer } from "../../customerSchema";
+import { useUpdateCustomer } from "../../hooks/customer_hooks";
 const PersonalInfoTab = () => {
   const { isEditing, setEditing } = useContext(EditingContext);
   const customer = useAuthStore((state) => state.customer).customer;
   const address = customer?.address[0];
 
+  const { mutate, isPending } = useUpdateCustomer();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm({
+    resolver: zodResolver(updateCustomer),
+    mode: "onChange",
+  });
+
+  const onSubmit = (data) => {
+    mutate(data, {
+      onSuccess: () => {
+        setEditing(false);
+      },
+      onError: (error) => {
+        if (error.response?.data?.errors) {
+          error.response.data.errors.forEach((err) => {
+            setError(err.field, {
+              type: "server",
+              message: err.message,
+            });
+          });
+        } else if (error.request) {
+          // Request made but no response
+          toast.error("Server not responding");
+        } else {
+          // Something else
+          toast.error("Unexpected error occurred");
+        }
+      },
+    });
+  };
   const handleClick = () => {
-    setEditing(false);
+    if (!isPending) return;
   };
 
   return (
@@ -31,80 +72,122 @@ const PersonalInfoTab = () => {
         </CardDescription>
       </CardHeader>
       <CardContent className="space-y-6">
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-          <div className="space-y-2">
-            <Label htmlFor="name">Name</Label>
-            <Input
-              id="name"
-              defaultValue={customer?.name}
-              disabled={!isEditing}
-            />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+            <div className="space-y-2">
+              <Label htmlFor="name">Name</Label>
+              <Input
+                {...register("name")}
+                id="name"
+                defaultValue={customer?.name}
+                disabled={!isEditing}
+                className={cn(
+                  errors?.name && "border-red-500 focus-visible:ring-red-500",
+                )}
+              />
+              <FormFieldError error={errors.name} helper="" />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                // {...register("email")}
+                id="email"
+                type="email"
+                defaultValue={customer?.user?.email}
+                disabled
+                // className={cn(
+                //   errors?.email && "border-red-500 focus-visible:ring-red-500",
+                // )}
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="phone">Phone</Label>
+              <Input
+                {...register("phone")}
+                id="phone"
+                defaultValue={customer?.phone}
+                disabled={!isEditing}
+                className={cn(
+                  errors?.phone && "border-red-500 focus-visible:ring-red-500",
+                )}
+              />
+              <FormFieldError error={errors.phone} helper="" />
+            </div>
           </div>
-          <div className="space-y-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              defaultValue={customer?.user?.email}
-              disabled={!isEditing}
-            />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="phone">Phone</Label>
-            <Input
-              id="phone"
-              defaultValue={customer?.phone}
-              disabled={!isEditing}
-            />
-          </div>
-        </div>
 
-        <Separator />
-        {/* ADDRESS */}
-        <h2 className="font-semibold">Address</h2>
+          <Separator />
+          {/* ADDRESS */}
+          <h2 className="font-semibold">Address</h2>
 
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-          <div className="space-y-2">
-            <Label htmlFor="street">Street</Label>
-            <Input
-              id="street"
-              defaultValue={address?.street}
-              disabled={!isEditing}
-            />
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+            <div className="space-y-2">
+              <Label htmlFor="street">Street</Label>
+              <Input
+                {...register("address.street")}
+                id="street"
+                defaultValue={address?.street}
+                disabled={!isEditing}
+                className={cn(
+                  errors?.address?.street &&
+                    "border-red-500 focus-visible:ring-red-500",
+                )}
+              />
+              <FormFieldError error={errors?.address?.street} helper="" />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="city">City</Label>
+              <Input
+                {...register("address.city")}
+                id="city"
+                defaultValue={address?.city}
+                disabled={!isEditing}
+                className={cn(
+                  errors?.address?.city &&
+                    "border-red-500 focus-visible:ring-red-500",
+                )}
+              />
+              <FormFieldError error={errors?.address?.city} helper="" />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="zipCode">ZipCode</Label>
+              <Input
+                {...register("address.zipCode")}
+                id="zipCode"
+                type="text"
+                inputMode="numeric"
+                pattern="\d*"
+                defaultValue={address?.zipCode}
+                disabled={!isEditing}
+                className={`[&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none appearance-none ${
+                  errors?.address?.zipCode &&
+                  "border-red-500 focus-visible:ring-red-500"
+                }`}
+              />
+              <FormFieldError error={errors?.address?.zipCode} helper="" />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="country">Country</Label>
+              <Input
+                {...register("address.country")}
+                id="country"
+                defaultValue={address?.country}
+                disabled={!isEditing}
+                className={cn(
+                  errors?.address?.country &&
+                    "border-red-500 focus-visible:ring-red-500",
+                )}
+              />
+              <FormFieldError error={errors?.address?.country} helper="" />
+            </div>
           </div>
-          <div className="space-y-2">
-            <Label htmlFor="city">City</Label>
-            <Input
-              id="city"
-              defaultValue={address?.city}
-              disabled={!isEditing}
-            />
+          <div
+            className="float-right justify-end"
+            hidden={!isEditing}
+            // onClick={handleClick}
+          >
+            <Button type="submit">{isPending ? "Updating..." : "Save"}</Button>
           </div>
-          <div className="space-y-2">
-            <Label htmlFor="zipCode">ZipCode</Label>
-            <Input
-              id="zipCode"
-              type="number"
-              defaultValue={address?.zipCode}
-              disabled={!isEditing}
-              className="[&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none appearance-none"
-            />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="country">Country</Label>
-            <Input
-              id="country"
-              defaultValue={address?.country}
-              disabled={!isEditing}
-            />
-          </div>
-        </div>
-        <div
-          className="float-right justify-end"
-          hidden={!isEditing}
-          onClick={handleClick}>
-          <Button>Save</Button>
-        </div>
+        </form>
       </CardContent>
     </Card>
   );
