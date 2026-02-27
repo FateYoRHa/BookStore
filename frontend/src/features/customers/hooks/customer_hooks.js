f3f8261dd6291548ed4c84f554274aa78bf9916e
FateYoRHa <fagyan.att@gmail.com>
2026-02-27 10:26:47 +0800

    feat: add update customer hook

--- frontend/src/features/customers/hooks/customer_hooks.js
+++ frontend/src/features/customers/hooks/customer_hooks.js
@@ -1,6 +1,8 @@
-import { getCustomerRequest } from "../api/customer";
-import { useQuery } from "@tanstack/react-query";
+import { getCustomerRequest, updateCustomerRequest } from "../api/customer";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useAuthStore } from "@/features/auth/store/authStore";
+import toast from "react-hot-toast";
+
 export const useGetCustomer = () => {
   // get user
   const user = useAuthStore((state) => state.user);
@@ -10,5 +12,17 @@ export const useGetCustomer = () => {
     enabled: !!user, // only runs when logged in
     staleTime: 1000 * 60 * 2,
     refetchOnWindowFocus: false,
+  });
+};
+
+export const useUpdateCustomer = () => {
+  const customer = useAuthStore((state) => state.setCustomer);
+  return useMutation({
+    mutationFn: updateCustomerRequest,
+
+    onSuccess: () => {
+      customer();
+      toast.success("Profile updated successfully");
+    },
   });
 };
