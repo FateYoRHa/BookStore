4e58bcffcaff9b486864ea8821b85ef0183e9bb9
FateYoRHa <fagyan.att@gmail.com>
2026-03-11 05:34:53 +0800

    feat: add category card skeleton

--- frontend/src/features/categories/pages/components/CategorySkeleton.jsx
+++ frontend/src/features/categories/pages/components/CategorySkeleton.jsx
@@ -1,8 +1,24 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 
-const CategorySkeleton = () => {
+export default function CategorySkeleton() {
   return (
-    <div>CategorySkeleton</div>
-  )
-}
+    <Card className="w-full">
+      <CardContent className="p-4 flex flex-col items-center space-y-2">
+        {/* Image Skeleton */}
+        <div className="w-full">
+          <AspectRatio ratio={1 / 1}>
+            <Skeleton className="h-full w-full rounded-md" />
+          </AspectRatio>
+        </div>
 
-export default CategorySkeleton
+        {/* Category Name */}
+        <Skeleton className="h-5 w-24" />
+
+        {/* Description */}
+        <Skeleton className="h-4 w-32" />
+      </CardContent>
+    </Card>
+  );
+}
