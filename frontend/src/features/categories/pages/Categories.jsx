fc63f7c888c1a1ab8a241cee393c387260a85c33
FateYoRHa <fagyan.att@gmail.com>
2026-03-11 05:34:35 +0800

    feat: display category cards and add loading skeleton

--- frontend/src/features/categories/pages/Categories.jsx
+++ frontend/src/features/categories/pages/Categories.jsx
@@ -1,7 +1,43 @@
-
+import { useGetCategories } from "../hooks/category_hooks";
+import CategorySkeleton from "./components/CategorySkeleton";
+import CategoryCard from "./components/CategoryCard";
 const Categories = () => {
+  const { data: categories, isLoading } = useGetCategories();
+  return (
+    <div className="container max-w-7xl mx-auto px-4 py-8">
+      {/* ================= Header SECTION ================= */}
+      <section className="container flex flex-col items-center text-center mx-auto px-4 py-5">
+        <h1 className="text-2xl md:text-4xl font-bold tracking-tight">
+          Discover Your Next Favorite Book
+        </h1>
+      </section>
 
-  return <div>Categories</div>;
+      {/* Categories Grid */}
+      {isLoading ? (
+        <div
+          className="grid gap-5 
+                    sm:grid-cols-2 
+                    md:grid-cols-3 
+                    lg:grid-cols-4 
+                    xl:grid-cols-6 ">
+          {Array.from({ length: 16 }).map((_, i) => (
+            <CategorySkeleton key={i} />
+          ))}
+        </div>
+      ) : (
+        <div
+          className="grid gap-5 
+                    sm:grid-cols-2 
+                    md:grid-cols-3 
+                    lg:grid-cols-4 
+                    xl:grid-cols-6 ">
+          {categories.map((category) => (
+            <CategoryCard key={category._id} category={category} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Categories;
