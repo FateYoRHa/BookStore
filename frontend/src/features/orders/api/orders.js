5f46c67d5c7e95e2b8920656dfaeccd213a5c8bb
FateYoRHa <fagyan.att@gmail.com>
2026-03-10 13:59:57 +0800

    feat: add get customer orders api request

--- frontend/src/features/orders/api/orders.js
+++ frontend/src/features/orders/api/orders.js
@@ -4,3 +4,8 @@ export const checkoutRequest = async (data) => {
   const response = await api.post("/commerce/payment", data);
   return response.data;
 };
+
+export const getCustomerOrdersRequest = async () => {
+  const response = await api.get("/customer/orders")
+  return response.data
+}
