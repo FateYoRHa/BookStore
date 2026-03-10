90e05a0dcd7177a3912af2dda933b63ca22ab8f7
FateYoRHa <fagyan.att@gmail.com>
2026-03-10 13:59:27 +0800

    feat: add get customer order hook

--- frontend/src/features/orders/hooks/orders_hook.js
+++ frontend/src/features/orders/hooks/orders_hook.js
@@ -1,8 +1,20 @@
-import { checkoutRequest } from "../api/orders";
-import { useMutation } from "@tanstack/react-query";
+import { checkoutRequest, getCustomerOrdersRequest } from "../api/orders";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useAuthStore } from "@/features/auth/store/authStore";
 
 export const useCheckout = () => {
   return useMutation({
     mutationFn: checkoutRequest,
+  });
+};
+
+export const useGetCustomerOrders = () => {
+  const user = useAuthStore((state) => state.user);
+  return useQuery({
+    queryKey: ["orders"],
+    queryFn: () => getCustomerOrdersRequest(),
+    enabled: !!user, // only runs when logged in
+    staleTime: 1000 * 60 * 2,
+    refetchOnWindowFocus: false,
   });
 };
