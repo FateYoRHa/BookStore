fd77c9187e01904976f53cb1bdf7e867f30c1f2c
FateYoRHa <fagyan.att@gmail.com>
2026-03-12 03:53:57 +0800

    feat: add getAuthorRequest api

--- frontend/src/features/authors/api/authors.js
+++ frontend/src/features/authors/api/authors.js
@@ -1,1 +1,8 @@
 import api from "@/services/axios";
+
+export const getAuthorsRequest = async ({ page, search, limit }) => {
+  const response = await api.get("/core/authors", {
+    params: { page, search, limit },
+  });
+  return response.data;
+};
