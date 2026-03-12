a2fcf72faecc0de3f12a9d94345bdf787a828d06
FateYoRHa <fagyan.att@gmail.com>
2026-03-12 23:18:25 +0800

    feat: add getAuthor hook

--- frontend/src/features/authors/hooks/author_hooks.js
+++ frontend/src/features/authors/hooks/author_hooks.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getAuthorsRequest } from "../api/authors";
+import { getAuthorsRequest, getAuthorRequest } from "../api/authors";
 
 /*
 Fetch authors with pagination + search
@@ -8,11 +8,6 @@ Fetch authors with pagination + search
 export const useGetAuthors = ({ page, search, limit = 12 }) => {
   return useQuery({
     queryKey: ["authors", page, search, limit],
-
-    /*
-    queryFn runs when queryKey changes
-    (page or search change)
-    */
     queryFn: () =>
       getAuthorsRequest({
         page,
@@ -21,5 +16,15 @@ export const useGetAuthors = ({ page, search, limit = 12 }) => {
       }),
 
     keepPreviousData: true, // prevents UI flicker during page change
+  });
+};
+export const useGetAuthor = (id) => {
+  return useQuery({
+    queryKey: ["authors", id],
+    queryFn: () => getAuthorRequest(id),
+    keepPreviousData: true,
+    enabled: !!id,
+    staleTime: 1000 * 60 * 2,
+    refetchOnWindowFocus: false,
   });
 };
