6d37543f4ba37ad2f9b446b75b9ad7fcce737300
FateYoRHa <fagyan.att@gmail.com>
2026-03-16 08:37:13 +0800

    feat: add update review request api

--- frontend/src/features/reviews/api/reviews.js
+++ frontend/src/features/reviews/api/reviews.js
@@ -4,3 +4,7 @@ export const postReviewRequest = async (review) => {
   const response = await api.post("/engagement/review", review);
   return response.data;
 };
+export const updateReviewRequest = async (review) => {
+  const response = await api.put("/engagement/review", review);
+  return response.data;
+};
