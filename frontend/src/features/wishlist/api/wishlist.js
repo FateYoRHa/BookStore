d9c55fd2e3eff524f1823a3075ac94a0448e75d7
FateYoRHa <fagyan.att@gmail.com>
2026-03-18 13:02:58 +0800

    feat(wishlist): add API request for getting wishlist

--- frontend/src/features/wishlist/api/wishlist.js
+++ frontend/src/features/wishlist/api/wishlist.js
@@ -4,3 +4,8 @@ export const addWishlistRequest = async (book) => {
   const response = await api.put("/engagement/wishlist", book);
   return response.data;
 };
+
+export const getWishlistRequest = async () => {
+  const response = await api.get("/engagement/wishlist");
+  return response.data;
+};
