d07bdbddf7ce79f91f84dac590664fd7ee9ea551
FateYoRHa <fagyan.att@gmail.com>
2026-03-18 13:02:09 +0800

    refactor(wishlist): add get wishlist reusable hook

--- frontend/src/features/wishlist/hooks/wishlist_hooks.js
+++ frontend/src/features/wishlist/hooks/wishlist_hooks.js
@@ -1,6 +1,8 @@
-import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { useQueryClient, useMutation, useQuery } from "@tanstack/react-query";
 import { toast } from "sonner";
-import { addWishlistRequest } from "../api/wishlist";
+import { addWishlistRequest, getWishlistRequest } from "../api/wishlist";
+import { useAuthStore } from "@/features/auth/store/authStore";
+
 export const useAddWishlist = () => {
   const queryClient = useQueryClient();
 
@@ -11,5 +13,17 @@ export const useAddWishlist = () => {
       queryClient.invalidateQueries(["wishlist"]);
       toast.success("Book added to wishlist.");
     },
+  });
+};
+
+export const useGetWishlist = () => {
+  // get user
+  const user = useAuthStore((state) => state.user);
+  return useQuery({
+    queryKey: ["wishlist"],
+    queryFn: () => getWishlistRequest(),
+    enabled: !!user, // only runs when logged in
+    staleTime: 1000 * 60 * 2,
+    refetchOnWindowFocus: false,
   });
 };
