90e141f2a3a5d280817c71dfdcd00b93a52f83fb
FateYoRHa <fagyan.att@gmail.com>
2026-03-21 11:56:43 +0800

    refactor(dashboard): update dashboard ui

--- frontend/src/features/admin/dashboard/AdminDashboard.jsx
+++ frontend/src/features/admin/dashboard/AdminDashboard.jsx
@@ -1,7 +1,22 @@
+import DashboardSidebar from "./components/DashboardSidebar";
+import DashboardCharts from "./components/DashboardCharts";
+import DashboardHeader from "./components/DashboardHeader";
+import DashboardCards from "./components/DashboardCards";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 const AdminDashboard = () => {
   return (
-    <div>AdminDashboard</div>
-  )
-}
+    <div className="flex flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          <DashboardCards />
+          <div className="px-4 lg:px-6">
+            <DashboardCharts />
+          </div>
+          {/* <DataTable data={data} /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
