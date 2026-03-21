0dc1a83cd4685d7436f1b89c490dabc4decc0184
FateYoRHa <fagyan.att@gmail.com>
2026-03-21 11:55:32 +0800

    refactor(router): add role-based route

--- frontend/src/app/Router.jsx
+++ frontend/src/app/Router.jsx
@@ -1,7 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import AdminRoutes from "./AdminRoutes";
+import AdminRoutes from "./RouteGuard";
 import LoginPage from "../features/auth/pages/AuthPage";
 
+// PUBLIC PAGES
+import MainLayout from "./MainLayout";
 import HomePage from "@/features/homepage/pages/HomePage";
 
 import Books from "@/features/books/pages/Books";
@@ -23,50 +25,106 @@ import Categories from "@/features/categories/pages/Categories";
 
 import CustomerWishlist from "@/features/wishlist/pages/CustomerWishlist";
 
+// ADMIN PAGES
+import AdminLayout from "./AdminLayout";
+import RouteGuard from "./RouteGuard";
+import AdminDashboard from "@/features/admin/dashboard/AdminDashboard";
+import BooksTable from "@/features/admin/books/BooksTable";
+import AuthorsTable from "@/features/admin/authors/AuthorsTable";
+import CategoriesTable from "@/features/admin/categories/CategoriesTable";
+import OrdersTable from "@/features/admin/orders/OrdersTable";
+
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      {/* ========================== PUBLIC ROUTES ========================== */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
 
-      <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
 
-      <Route
-        path="/profile"
-        element={
-          <AdminRoutes>
-            <Profile />
-          </AdminRoutes>
-        }
-      />
+        <Route
+          path="/profile"
+          element={
+            <RouteGuard>
+              <Profile />
+            </RouteGuard>
+          }
+        />
 
-      <Route
-        path="/cart"
-        element={
-          <AdminRoutes>
-            <Cart />
-          </AdminRoutes>
-        }
-      />
-      <Route path="/customer/orders" element={<CustomerOrders />} />
+        <Route
+          path="/cart"
+          element={
+            <RouteGuard>
+              <Cart />
+            </RouteGuard>
+          }
+        />
+        <Route path="/customer/orders" element={<CustomerOrders />} />
 
-      <Route path="/categories" element={<Categories />} />
-      <Route
-        path="/wishlist"
-        element={
-          <AdminRoutes>
-            <CustomerWishlist />
-          </AdminRoutes>
-        }
-      />
+        <Route path="/categories" element={<Categories />} />
+        <Route
+          path="/wishlist"
+          element={
+            <RouteGuard>
+              <CustomerWishlist />
+            </RouteGuard>
+          }
+        />
 
-      <Route path="/books" element={<Books />} />
-      <Route path="/books/:id" element={<BookDetails />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/books/:id" element={<BookDetails />} />
 
-      <Route path="/authors" element={<Authors />} />
-      <Route path="/authors/:id" element={<AuthorDetails />} />
+        <Route path="/authors" element={<Authors />} />
+        <Route path="/authors/:id" element={<AuthorDetails />} />
+        <Route path="/checkout/success" element={<PaymentSuccess />} />
+        <Route path="/checkout/failed" element={<PaymentFailed />} />
+      </Route>
 
-      <Route path="/checkout/success" element={<PaymentSuccess />} />
-      <Route path="/checkout/failed" element={<PaymentFailed />} />
+      {/* ========================== ADMIN ROUTES ========================== */}
+
+      <Route element={<AdminLayout />}>
+        <Route
+          path="/admin/dashboard"
+          element={
+            <RouteGuard>
+              <AdminDashboard />
+            </RouteGuard>
+          }
+        />
+        <Route
+          path="/admin/books"
+          element={
+            <RouteGuard>
+              <BooksTable />
+            </RouteGuard>
+          }
+        />
+        <Route
+          path="/admin/authors"
+          element={
+            <RouteGuard>
+              <AuthorsTable />
+            </RouteGuard>
+          }
+        />
+        <Route
+          path="/admin/category"
+          element={
+            <RouteGuard>
+              <CategoriesTable />
+            </RouteGuard>
+          }
+        />
+        <Route
+          path="/admin/orders"
+          element={
+            <RouteGuard>
+              <OrdersTable />
+            </RouteGuard>
+          }
+        />
+      </Route>
     </Routes>
   );
 }
