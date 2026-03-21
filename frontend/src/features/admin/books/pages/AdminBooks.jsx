a9d15ad4a63d04edac06cc88a74f9080fa5aa32f
FateYoRHa <fagyan.att@gmail.com>
2026-03-21 15:16:54 +0800

    feat(admin): add books page

---
+++ frontend/src/features/admin/books/pages/AdminBooks.jsx
@@ -0,0 +1,11 @@
+import BooksTable from "./components/BooksTable";
+
+const AdminBooks = () => {
+  return (
+    <div className="flex-1 overflow-auto p-4">
+      <BooksTable />
+    </div>
+  );
+};
+
+export default AdminBooks;
