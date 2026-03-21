9e31c2f76ad5a9791f7a6cd944d495bd029ad856
FateYoRHa <fagyan.att@gmail.com>
2026-03-21 14:18:46 +0800

    feat(admin): extract and get current route title from adminRoutes dynamic dashboard header title/header

--- frontend/src/features/admin/dashboard/components/DashboardHeader.jsx
+++ frontend/src/features/admin/dashboard/components/DashboardHeader.jsx
@@ -1,7 +1,14 @@
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-
+import { useLocation } from "react-router-dom";
+import { adminRoutes } from "../util/adminRoutes";
 const DashboardHeader = () => {
+  const location = useLocation();
+
+  const currentRoute = adminRoutes.find((route) =>
+    location.pathname.startsWith(route.url),
+  );
+  const title = currentRoute?.title || "Dashboard";
   return (
     <header className="flex h-(--header-height) items-center border-b border-border bg-background/80 backdrop-blur">
       <div className="flex w-full items-center gap-2 px-4 lg:px-6">
@@ -9,7 +16,7 @@ const DashboardHeader = () => {
 
         <Separator orientation="vertical" className="mx-2 h-4 bg-border" />
 
-        <h1 className="text-sm font-semibold text-foreground">Dashboard</h1>
+        <h1 className="text-sm font-semibold text-foreground">{title}</h1>
       </div>
     </header>
   );
