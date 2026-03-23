17faf8af186b11a11b52737ee5a876187fbfe188
FateYoRHa <fagyan.att@gmail.com>
2026-03-23 14:15:24 +0800

    feat(admin-books): add addBook schema for new book creation with validation

--- frontend/src/features/admin/books/bookSchema.js
+++ frontend/src/features/admin/books/bookSchema.js
@@ -56,3 +56,60 @@ export const updateBook = z.object({
     .optional(),
   bookCode: z.string(),
 });
+export const addBook = z.object({
+  title: z
+    .string()
+    .trim()
+    .min(2, "Title must have at least 2 characters.")
+    .max(100, "Title exceeds 100 characters."),
+
+  description: z
+    .string()
+    .trim()
+    .max(1500, "Book description exceeds character limit.")
+    .optional(),
+
+  author: objectId,
+
+  publisher: z
+    .string()
+    .trim()
+    .min(2, "Publisher must be valid.")
+    .max(100, "Publisher exceeds character limit."),
+
+  publicationDate: z.coerce.date({
+    invalid_type_error: "Invalid date format.",
+  }),
+
+  price: z.coerce
+    .number()
+    .refine((val) => Number.isInteger(Math.round(val * 100)), {
+      message: "Max 2 decimal places",
+    })
+    .min(0, "Price must be a positive number.")
+    .max(100000, "Price exceeds allowed limit."),
+
+  categories: z
+    .array(objectId)
+    .min(1, "At least one category is required.")
+    .max(10, "Too many categories."),
+  images: z
+    .array(
+      z
+        .instanceof(File)
+        .refine((file) => file.size <= 5 * 1024 * 1024, "Max 5MB")
+        .refine(
+          (file) =>
+            ["image/jpeg", "image/png", "image/webp"].includes(file.type),
+          "Invalid file type",
+        ),
+    )
+    .optional(),
+  quantity: z.coerce.number().int().min(1, "Quantity must be greater than 0."),
+  pages: z.coerce.number().int().min(50, "Book must have atleast 50 pages."),
+  language: z
+    .string()
+    .trim()
+    .min(2, "Language must be valid.")
+    .max(50, "Language exceeds character limit."),
+});
