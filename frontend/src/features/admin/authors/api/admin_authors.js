ee245a015b8d082daa48bcd00683d09932cb0943
FateYoRHa <fagyan.att@gmail.com>
2026-03-24 17:27:09 +0800

    feat(admin-authors):add update and add author api request

--- frontend/src/features/admin/authors/api/admin_authors.js
+++ frontend/src/features/admin/authors/api/admin_authors.js
@@ -9,3 +9,12 @@ export const getAdminAuthorsList = async () => {
   const response = await api.get("/admin/authors/list");
   return response.data;
 };
+export const addAdminAuthor = async (author) => {
+  const response = await api.post("/admin/authors", author);
+  return response.data;
+};
+
+export const updateAdminAuthor = async (author) => {
+  const response = await api.put(`/admin/authors/${author.authorCode}`, author);
+  return response.data;
+};
