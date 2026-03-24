fdc0f8249e9343767e8b739526b4ccc1bd5a3b8a
FateYoRHa <fagyan.att@gmail.com>
2026-03-24 16:07:15 +0800

    refactor(admin-authors): add get authors hook

--- frontend/src/features/admin/authors/hooks/admin_author_hooks.js
+++ frontend/src/features/admin/authors/hooks/admin_author_hooks.js
@@ -1,9 +1,25 @@
-import { useQuery } from "@tanstack/react-query";
-import { getAdminAuthorsList } from "../api/admin_authors";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getAdminAuthors, getAdminAuthorsList } from "../api/admin_authors";
 
 export const useGetAdminAuthorsList = () => {
+  const queryClient = useQueryClient();
   return useQuery({
     queryKey: ["authors"],
     queryFn: () => getAdminAuthorsList(),
+    onSuccess: (data) => {
+      queryClient.setQueryData(["authors"], data);
+    },
+  });
+};
+
+export const useGetAdminAuthors = () => {
+  const queryClient = useQueryClient();
+
+  return useQuery({
+    queryKey: ["authors"],
+    queryFn: () => getAdminAuthors(),
+    onSuccess: (data) => {
+      queryClient.setQueryData(["authors"], data);
+    },
   });
 };
