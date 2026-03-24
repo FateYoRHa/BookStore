c038c4e3068a71b8a37cbbeabce7382ec389995f
FateYoRHa <fagyan.att@gmail.com>
2026-03-24 17:26:22 +0800

    feat(admin-authors): add update and add mutation hooks

--- frontend/src/features/admin/authors/hooks/admin_author_hooks.js
+++ frontend/src/features/admin/authors/hooks/admin_author_hooks.js
@@ -1,5 +1,10 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { getAdminAuthors, getAdminAuthorsList } from "../api/admin_authors";
+import {
+  addAdminAuthor,
+  getAdminAuthors,
+  getAdminAuthorsList,
+  updateAdminAuthor,
+} from "../api/admin_authors";
 
 export const useGetAdminAuthorsList = () => {
   const queryClient = useQueryClient();
@@ -20,6 +25,26 @@ export const useGetAdminAuthors = () => {
     queryFn: () => getAdminAuthors(),
     onSuccess: (data) => {
       queryClient.setQueryData(["authors"], data);
+    },
+  });
+};
+
+export const useAddAdminAuthor = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (author) => addAdminAuthor(author),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["authors"]);
+    },
+  });
+};
+
+export const useUpdateAdminAuthor = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (author) => updateAdminAuthor(author),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["authors"]);
     },
   });
 };
