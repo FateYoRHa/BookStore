8c01a651eba4d1c8dadf144cea0c390d3cefbe44
FateYoRHa <fagyan.att@gmail.com>
2026-03-25 09:13:10 +0800

    feat(admin-author): implement remove and restore/readd admin author functionality

--- frontend/src/features/admin/authors/pages/AdminAuthors.jsx
+++ frontend/src/features/admin/authors/pages/AdminAuthors.jsx
@@ -3,36 +3,41 @@ import DataTable from "../../components/DataTable";
 import AddAuthor from "./components/AddAuthor";
 import EditAuthor from "./components/EditAuthor";
 import AuthorTableColumns from "./components/AuthorTableColumns";
-import { useGetAdminAuthors } from "../hooks/admin_author_hooks";
+import {
+  useGetAdminAuthors,
+  useReAddAdminAuthor,
+  useRemoveAdminAuthor,
+} from "../hooks/admin_author_hooks";
 import { User2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 const AdminAuthors = () => {
   const [openAddForm, setOpenAddForm] = useState(false);
   const [openEditForm, setOpenEditForm] = useState(false);
   const [editAuthor, setEditAuthor] = useState(null);
+  const { mutate: removeAuthor, isPending: isRemoving } =
+    useRemoveAdminAuthor();
+  const { mutate: reAddAuthor, isPending: isReAdding } = useReAddAdminAuthor();
+
   const handleAddAuthor = () => {
     setOpenAddForm(true);
   };
   const handleEditAuthor = (author) => {
-    console.log("EDIT: ", author);
     setEditAuthor(author);
     setOpenEditForm(true);
   };
   const handleDeleteAuthor = (author) => {
-    console.log("DELETE: ", author);
-    // TODO delete author
+    removeAuthor(author);
   };
   const handleReAddAuthor = (author) => {
-    console.log("RE-ADD: ", author);
-    // TODO re-add author
+    reAddAuthor(author);
   };
   const { data } = useGetAdminAuthors();
   const column = AuthorTableColumns({
     onEdit: handleEditAuthor,
     onDelete: handleDeleteAuthor,
     onReAdd: handleReAddAuthor,
-    isDeleting: true,
-    isReAdding: true,
+    isDeleting: isRemoving,
+    isReAdding: isReAdding,
   });
   return (
     <div className="flex flex-col flex-1 p-4 overflow-auto min-w-0">
