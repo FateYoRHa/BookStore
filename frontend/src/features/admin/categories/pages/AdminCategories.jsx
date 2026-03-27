ba6c5b2a95055930362be86fd8a92d3578deb1d8
FateYoRHa <fagyan.att@gmail.com>
2026-03-27 11:38:13 +0800

    feat(admin-category): replace placeholder to display categories
    - extract data from useGetCategories hook
    - extract column from CategoryTableColumns
    - import and implement DataTable passing data and columns to display table

--- frontend/src/features/admin/categories/pages/AdminCategories.jsx
+++ frontend/src/features/admin/categories/pages/AdminCategories.jsx
@@ -1,7 +1,15 @@
-const AdminCategories = () => {
-  return (
-    <div>AdminCategories</div>
-  )
-}
+import { useGetCategories } from "@/features/categories/hooks/category_hooks";
+import DataTable from "../../components/DataTable";
+import CategoryTableColumns from "./components/CategoryTableColumns";
 
-export default AdminCategories
+const AdminCategories = () => {
+  const { data } = useGetCategories();
+  const columns = CategoryTableColumns();
+  return (
+    <div className="flex flex-col flex-1 p-4 overflow-auto min-w-0">
+      <DataTable data={data} columns={columns} />
+    </div>
+  );
+};
+
+export default AdminCategories;
