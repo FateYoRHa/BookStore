135b8514010b253fa9d5151e49b60a67f720fa66
FateYoRHa <fagyan.att@gmail.com>
2026-03-27 12:47:56 +0800

    feat(admin-category): add open AddCategory button

--- frontend/src/features/admin/categories/pages/AdminCategories.jsx
+++ frontend/src/features/admin/categories/pages/AdminCategories.jsx
@@ -1,13 +1,30 @@
 import { useGetCategories } from "@/features/categories/hooks/category_hooks";
 import DataTable from "../../components/DataTable";
 import CategoryTableColumns from "./components/CategoryTableColumns";
+import { useState } from "react";
+import AddCategory from "./components/AddCategory";
+import { BookmarkPlus } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const AdminCategories = () => {
+  const [openAddForm, setOpenAddForm] = useState(false);
   const { data } = useGetCategories();
   const columns = CategoryTableColumns();
+
+  // HANDLERS
+  const handleAddCategory = () => {
+    setOpenAddForm(true);
+  };
   return (
     <div className="flex flex-col flex-1 p-4 overflow-auto min-w-0">
+      <Button onClick={handleAddCategory} className="self-end mb-4">
+        <BookmarkPlus className="h-4 w-4" />
+        Add Category
+      </Button>
       <DataTable data={data} columns={columns} />
+
+      {/* ADD CATEGORY FORM */}
+      <AddCategory open={openAddForm} setOpen={setOpenAddForm} />
     </div>
   );
 };
