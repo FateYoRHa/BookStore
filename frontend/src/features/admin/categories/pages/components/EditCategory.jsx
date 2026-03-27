4423d9624ef104e8d6c7cf23dd43930d963ba5c0
FateYoRHa <fagyan.att@gmail.com>
2026-03-28 03:01:32 +0800

    feat(update-category-form): add update funcitonality
    - display row category data to form
    - delete uploaded images from cloudinary on error to avoid isolated assets

--- frontend/src/features/admin/categories/pages/components/EditCategory.jsx
+++ frontend/src/features/admin/categories/pages/components/EditCategory.jsx
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
-
 import {
   Dialog,
   DialogContent,
@@ -9,42 +11,184 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-const EditCategory = () => {
+import { useUpdateAdminCategory } from "../../hooks/admin_category_hooks";
+
+import FormFieldError from "@/components/forms/FormFieldError";
+import { updateCategorySchema } from "../../categorySchema";
+import { cn } from "@/lib/utils";
+import { deleteImages, uploadImages } from "@/services/cloudinaryImages";
+import { toast } from "sonner";
+import { Spinner } from "@/components/ui/spinner";
+const EditCategory = ({ open, setOpen, category }) => {
+  const { mutate: editCategory } = useUpdateAdminCategory();
+  const [isPending, setIsPending] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    setValue,
+    reset,
+  } = useForm({
+    resolver: zodResolver(updateCategorySchema),
+    defaultValues: {
+      name: "",
+      description: "",
+      newImage: undefined,
+    },
+  });
+
+  // image stuff
+  const newImage = watch("newImage");
+  const [preview, setPreview] = useState(null);
+  // Load category data
+  useEffect(() => {
+    if (!category) return;
+
+    reset({
+      name: category.name || "",
+      description: category.description || "",
+      newImage: undefined,
+    });
+
+    setPreview(category?.image?.url || null);
+  }, [category, reset]);
+
+  // Handle new image preview (override existing)
+  useEffect(() => {
+    if (!newImage) return;
+
+    const url = URL.createObjectURL(newImage);
+    setPreview(url);
+
+    return () => URL.revokeObjectURL(url);
+  }, [newImage]);
+
+  // ON SUBMIT
+  const onSubmit = async (data) => {
+    setIsPending(true);
+    let url = category?.image;
+    if (data.newImage) {
+      const res = await uploadImages(data.newImage, "categories");
+      url = res.images[0];
+    }
+    const payload = { ...data, image: url, catCode: category.categoryCode };
+    editCategory(payload, {
+      onSuccess: () => {
+        setOpen(false);
+        toast.success("Category updated successfully.");
+      },
+      onError: async () => {
+        toast.error("Failed to update category. Please try again.");
+        await deleteImages([url.public_id]); // <- this will delete uploaded image on error
+      },
+      onSettled: () => setIsPending(false),
+    });
+  };
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent>
         <DialogHeader className="items-center">
-          <DialogTitle>Add Category</DialogTitle>
+          <DialogTitle>Update Category</DialogTitle>
           <DialogDescription>
-            Fill in the details to add new category.
+            Fill in the details to update category details.
           </DialogDescription>
         </DialogHeader>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div>
-            <Input placeholder="Category name" />
+            <Input
+              placeholder="Category name"
+              {...register("name")}
+              className={cn(
+                errors?.name && "border-red-500 focus-visible:ring-red-500",
+              )}
+            />
+
+            <FormFieldError error={errors?.name} />
           </div>
 
           {/* Description */}
           <div>
-            <Textarea placeholder="Description" rows={3} />
+            <Textarea
+              placeholder="Description"
+              rows={3}
+              {...register("description")}
+              className={cn(
+                errors?.description &&
+                  "border-red-500 focus-visible:ring-red-500",
+              )}
+            />
+            <FormFieldError error={errors?.description} />
           </div>
 
           {/* Image Upload */}
           <div>
             {/* File Input */}
-            <Input type="file" accept="image/*" />
+            <Input
+              type="file"
+              accept="image/*"
+              onChange={(e) =>
+                setValue("newImage", e.target.files?.[0], {
+                  shouldValidate: true,
+                })
+              }
+              className={cn(
+                errors?.newImage && "border-red-500 focus-visible:ring-red-500",
+              )}
+            />
+
+            {/* Error */}
+            <FormFieldError error={errors?.newImage} />
+            {/* Preview */}
+            {preview && (
+              <img
+                src={preview}
+                alt="Preview"
+                className="h-24 w-24 object-cover rounded-md border mt-2"
+              />
+            )}
+
+            {/* File name */}
+            {newImage && (
+              <p className="text-xs text-muted-foreground mt-1">
+                {newImage.name}
+              </p>
+            )}
+
             {/* Remove button */}
-            <Button type="button" variant="ghost" size="sm" className="mt-2">
-              Remove Image
-            </Button>
+            {(preview || newImage) && (
+              <Button
+                type="button"
+                variant="ghost"
+                size="sm"
+                className="mt-2"
+                onClick={() => {
+                  setValue("image", undefined, { shouldValidate: true });
+                  setPreview(null);
+                }}>
+                Remove Image
+              </Button>
+            )}
           </div>
 
           {/* Actions */}
           <div className="flex justify-end gap-2">
-            <Button type="button" variant="outline">
+            <Button
+              type="button"
+              variant="outline"
+              onClick={() => setOpen(false)}>
               Cancel
             </Button>
-            <Button type="submit">Add Category</Button>
+            <Button type="submit" disabled={isPending}>
+              {isPending ? (
+                <>
+                  <Spinner /> Updating...
+                </>
+              ) : (
+                "Update Category"
+              )}
+            </Button>
           </div>
         </form>
       </DialogContent>
