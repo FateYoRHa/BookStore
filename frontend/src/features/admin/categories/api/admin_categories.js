c530f55e3e554d91b230a7145499559a39dc35d2
FateYoRHa <fagyan.att@gmail.com>
2026-03-27 10:42:57 +0800

    feat(admin-category): add api endpoints available in backend

--- frontend/src/features/admin/categories/api/admin_categories.js
+++ frontend/src/features/admin/categories/api/admin_categories.js
@@ -1,1 +1,19 @@
 import api from "@/services/axios";
+
+export const addCategoryRequest = async (category) => {
+  const res = await api.post("/admin/categories", category);
+  return res.data;
+};
+
+export const updateCategoryRequest = async (category) => {
+  const res = await api.put(`/admin/categories/${category.catCode}`, category);
+  return res.data;
+};
+
+export const removeCategoryRequest = async (category) => {
+  const res = await api.delete(
+    `/admin/categories/${category.catCode}`,
+    category,
+  );
+  return res.data;
+};
