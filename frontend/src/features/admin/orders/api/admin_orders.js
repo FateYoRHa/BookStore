c800ec9e9b4d0b266a84047d49dca6c93e3da019
FateYoRHa <fagyan.att@gmail.com>
2026-03-28 08:39:50 +0800

    feat(admin-orders): add get orders api request and query hook

--- frontend/src/features/admin/orders/api/admin_orders.js
+++ frontend/src/features/admin/orders/api/admin_orders.js
@@ -1,1 +1,6 @@
 import api from "@/services/axios";
+
+export const getAdminOrdersRequest = async () => {
+  const res = await api.get("/admin/orders");
+  return res.data;
+};
--- frontend/src/features/admin/orders/hooks/admin_order_hooks.js
+++ frontend/src/features/admin/orders/hooks/admin_order_hooks.js
@@ -1,1 +1,11 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAdminOrdersRequest } from "../api/admin_orders";
+
+export const useGetAdminOrders = () => {
+  return useQuery({
+    queryKey: ["orders"],
+    queryFn: () => getAdminOrdersRequest(),
+    staleTime: 1000 * 60 * 2,
+    refetchOnWindowFocus: false,
+  });
+};
