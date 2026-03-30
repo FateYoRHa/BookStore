b54026d5a23ac5d2837bf58cfbed369fff281016
FateYoRHa <fagyan.att@gmail.com>
2026-03-30 11:35:20 +0800

    refactor(order-api-request): include order data to be passed

--- frontend/src/features/admin/orders/api/admin_orders.js
+++ frontend/src/features/admin/orders/api/admin_orders.js
@@ -8,7 +8,7 @@ export const getAdminOrderDetailRequest = async (orderCode) => {
   const res = await api.get(`/admin/order/detail/${orderCode}`);
   return res.data;
 };
-export const updateAdminOrderRequest = async (orderCode) => {
-  const res = await api.put(`/admin/orders/${orderCode}`);
+export const updateAdminOrderRequest = async (order) => {
+  const res = await api.put(`/admin/orders/${order.orderCode}`, order);
   return res.data;
 };
