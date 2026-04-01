b3cd5d527fe0f0b23ecb160365bf461c20e4a316
FateYoRHa <fagyan.att@gmail.com>
2026-04-02 04:57:10 +0800

    feat(add-featured): replace placeholder with Dialog Form

--- frontend/src/features/admin/featured/pages/components/AddFeaturedDialog.jsx
+++ frontend/src/features/admin/featured/pages/components/AddFeaturedDialog.jsx
@@ -1,7 +1,69 @@
-const AddFeaturedDialog = () => {
-  return (
-    <div>AddFeaturedDialog</div>
-  )
-}
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Field, FieldSet } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
-export default AddFeaturedDialog
+const AddFeaturedDialog = ({ open, setOpen, featured }) => {
+  return (
+    <div>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTitle>Feature Item</DialogTitle>
+        <DialogDescription>
+          Feature an Item, Book, an Author or Category
+        </DialogDescription>
+        <DialogContent>
+          <Field>
+            <FieldLabel>Type</FieldLabel>
+            <Input placeholder="Type" disabled />
+          </Field>
+          <Field>
+            <FieldLabel>Id</FieldLabel>
+            <Input />
+          </Field>
+          <Field>
+            <FieldLabel>Section</FieldLabel>
+            <Select>
+              <SelectTrigger>
+                <SelectValue placeholder="Select section" />
+              </SelectTrigger>
+
+              <SelectContent>
+                <SelectItem>Hero</SelectItem>
+
+                <SelectItem>New Arrivals</SelectItem>
+
+                <SelectItem>Most Popular</SelectItem>
+              </SelectContent>
+            </Select>
+          </Field>
+          <div className="flex flex-row">
+            <FieldSet>
+              <Field>
+                <FieldLabel>Start From</FieldLabel>
+                <Input type="date" />
+              </Field>
+              <span>to</span>
+              <Field>
+                <FieldLabel>Start From</FieldLabel>
+                <Input type="date" />
+              </Field>
+            </FieldSet>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default AddFeaturedDialog;
