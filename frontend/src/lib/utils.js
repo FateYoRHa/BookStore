3f416cad1d57ce65bcf34a2e430b0a50015e7067
FateYoRHa <fagyan.att@gmail.com>
2026-04-06 06:25:39 +0800

    feat(auth): add deviceId generator using built-in webCrypto
    - creates deviceId if not existing
    - fetch if exists

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,6 +1,17 @@
 import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
+}
+export function getOrCreateDeviceId() {
+  let deviceId = localStorage.getItem("deviceId");
+
+  if (!deviceId) {
+    // Use the built-in Web Crypto API to generate deviceId
+    deviceId = crypto.randomUUID();
+    localStorage.setItem("deviceId", deviceId);
+  }
+
+  return deviceId;
 }
