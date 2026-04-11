6799bbd4e254fa4653d9426e49709fd1b8ca3a10
FateYoRHa <fagyan.att@gmail.com>
2026-04-11 16:48:44 +0800

    feat(dashboard): refactor dashboard revenue and customer summary data structure for improved mapping

--- frontend/src/features/admin/dashboard/util/dashboardStats.js
+++ frontend/src/features/admin/dashboard/util/dashboardStats.js
@@ -1,18 +1,21 @@
-export const dashboardRevenue = (revenue) => ({
-  card1: {
+export const dashboardRevenue = (revenue) => [
+  {
     label: "Total Revenue",
     value: `₱${revenue?.revenue?.totalRevenue || 0}`,
   },
-  card2: {
+  {
     label: "This Year",
     value: `₱${revenue?.revenue?.thisYearRevenue || 0}`,
   },
-  card3: {
+  {
     label: "Last 6 Months",
     value: `₱${revenue?.revenue?.lastSixMonthsRevenue || 0}`,
   },
-  card4: { label: "Today", value: `₱${revenue?.revenue?.todayRevenue || 0}` },
-});
+  {
+    label: "Today",
+    value: `₱${revenue?.revenue?.todayRevenue || 0}`,
+  },
+];
 
 export const revenueChartData = (data) => {
   return data?.map((item) => ({
@@ -23,35 +26,35 @@ export const revenueChartData = (data) => {
   }));
 };
 
-export const dashboardCustomerSummary = (customers) => ({
-  card1: {
+export const dashboardCustomerSummary = (customers) => [
+  {
     label: "Total Customers",
     value: customers?.customerSummary?.totalCustomers || 0,
   },
-  card2: {
+  {
     label: "New Customers This Year",
     value: customers?.customerSummary?.newCustomersThisYear || 0,
   },
-  card3: {
+  {
     label: "New Customers Last 6 Months",
     value: customers?.customerSummary?.newCustomersLastSixMonths || 0,
   },
-  card4: {
+  {
     label: "New Customers Today",
     value: customers?.customerSummary?.newCustomersToday || 0,
   },
-  card5: {
+  {
     label: "Active Customers",
     value: customers?.customerSummary?.activeCustomers || 0,
   },
-  card6: {
+  {
     label: "Returning Customers",
     value: customers?.customerSummary?.returningCustomers || 0,
   },
-});
+];
 
 export const getCustomerSummaryChartData = (data) => {
-  console.log(data)
+  console.log(data);
   return data?.map((item) => ({
     date: item?.createdAt ? new Date(item.createdAt).toLocaleDateString() : "",
     data1: item?.createdAt ? 1 : 0,
